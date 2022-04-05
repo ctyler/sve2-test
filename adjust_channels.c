f719abc3f9167756e647988ddd79b11fcdc470be
Chris Tyler <chris@tylers.info>
2022-04-05 13:39:27 -0400

    Status at the end of 2022-04-05 class (with bug in implementation 4\!)

--- adjust_channels.c
+++ adjust_channels.c
@@ -43,6 +43,16 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
         float red_factor, float green_factor, float blue_factor) {
 
         printf("Using adjust_channels() implementation #1 - Naive (autovectorizable)\n");
+        
+/*
+
+        The image is stored in memory as pixels of 3 bytes, representing red/green/blue values.
+        Each of these values is multiplied by the corresponding adjustment factor, with 
+        saturation, and then stored back to the original memory location.
+        
+        This simple implementation causes int to float to int conversions.
+        
+*/
 
         for (int i = 0; i < x_size * y_size * 3; i += 3) {
                 image[i]   = MIN((float)image[i]   * red_factor,   255);
@@ -87,7 +97,7 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
                 p0              predicate register for all predicated operations (load/store)
                 
         Notes on the scatter/gather operations (load/store):
-                * The image data is stored in memory as groups of 3 bytes - red, green, blue
+                * The image data is stored in memory as pixels of 3 bytes - red, green, blue
                 * If N is the number of lanes, the LD3B instruction loads n*3 bytes into 3 vector registers,
                         taking every 3rd byte into the lanes of the first register, the following bytes into
                         the lanes of the second register, and the next bytes into the lanes of the third
@@ -125,6 +135,8 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
         int g = (int)((float)green_factor * 64.0);
         int b = (int)((float)blue_factor  * 64.0);
         printf("Adjusted factors: r:%d   g:%d   b:%d\n", r, g, b);
+        
+        // Variables with precalculated total array size and iterator
         int size = x_size * y_size * 3;
         int i = 0;
         
@@ -145,7 +157,7 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
                 // ============================== Start loop and fetch data                                             \n\
                 WHILELO p0.B, %[i], %[size]                             // set up predicate register p0                 \n\
     L1:                                                                                                                 \n\
-                LD3B {z0.b, z1.b, z2.b}, p0/z, [ %[array], %[i] ]       // get 3 vector registers, scatter by bytes     \n\
+                LD3B {z0.b, z1.b, z2.b}, p0/z, [ %[array], %[i] ]       // load 3 vector registers, scatter by bytes     \n\
                                                                                                                         \n\
                 // ----------------------------- RED channel                                                            \n\
                 // Process odd lanes                                                                                    \n\
@@ -218,7 +230,7 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
         This is a fixed-point SVE 2 implementation like the previous one,
         with the same basic principles (8-bit fixed-point channel adjustment
         factors, etc).
-        
+
         The difference is in how data is loaded and manipulated. In the 
         implementation above, LD3B/ST3B is used to separate channel data
         into three different vector registers (one for each channel). In
@@ -228,10 +240,10 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
         of three, so the stride through memory is adjusted to the highest 
         multiple of three that is less than or equal to the number of lanes
         available in the vector registers on the current system.
-        
+
         In other words, the data is in memory in the regular interleaved
         fashion:
-        
+
                 byte 0          pixel 0         red data
                 byte 1          pixel 0         green data
                 byte 2          pixel 0         blue data
@@ -239,10 +251,10 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
                 byte 4          pixel 1         green data
                 byte 5          pixel 1         blue data
                 ...
-                
+
         Which is loaded into a vector register and multiplied by the
-        channel factors which are in the same order:
-        
+        channel factors which are repeated in the same order:
+
                 byte 0                           channel factor for red
                 byte 1                           channel factor for green
                 byte 2                           channel factor for blue
@@ -250,7 +262,7 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
                 byte 4                           channel factor for green
                 byte 5                           channel factor for blue
                 ...
-                
+
         After processing (including handling saturation), the data is placed
         back into the original memory locations.
         
@@ -259,22 +271,26 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
         wrong for the second iteration through the loop. For example, in a 128-bit
         SVE2 implementation, the first vector register will process 16 lanes of data,
         as r/g/b,r/g/b,r/g/b,r/g/b,r/g/b,r/g/b,r/g/b,r -- note that this is 5 full pixels
-        plus the red channel of a 6th pixel. When the next 128 bits (16 bytes) of data
-        is fetched, it will start with a green data byte, not a red data byte.
+        (15 bytes) plus the red channel of a 6th pixel (16th byte). When the next 128 
+        bits (16 bytes) of data is fetched, it will start with a green data byte, not 
+        a red data byte.
         
         To rectify this, we have to cause the elements of the incomplete pixel to 
         remain unchanged, and then overlap the next load operation to start at the 
-        beginning of the incomplete pixel.
+        beginning of the incomplete pixel -- that is, the second register load will
+        start 15 bytes after the first register load.
+        
+        (Note that this will cause misaligned reads and writes).
         
         (Note that if the number of lanes in a vector register _is_ a multiple of 3,
-        such as in a 384-bit impelentation of SVE2, this extra fiddling is not required
+        such as in a 384-bit implementation of SVE2, this extra fiddling is not required
         and will disappear).
         
         To ensure that the incomplete pixel is unchanged, we can either manipulate the 
         predicate register, or we can simply set the scaling factor for the incomplete
         pixel elements to 1.0 (which is the approach used here).
 
-        Register usage is the same as the previous version, except:
+        Register usage is the same as the previous implementation above, except:
         
                 z0              interleaved pixel data
                 z1, z2          not used
@@ -305,7 +321,7 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
         uint64_t elements;
         __asm__ __volatile__ ("CNTB %[elements]" : [elements]"=r"(elements) : : );
         
-        // Figure out the largest number less than or equal to lanes that is a multiple of 3
+        // Figure out the largest number less than or equal to lane count that is a multiple of 3
         int elements3 = (elements / 3) * 3;
         
         // Set up the factor table for multiplication
@@ -370,11 +386,96 @@ void adjust_channels(unsigned char *image, int x_size, int y_size,
 // -------------------------------------------------------------------- ACLE Intrinsics
 #elif ADJUST_CHANNEL_IMPLEMENTATION == 4
 
+#include <arm_sve.h>
+
 void adjust_channels(unsigned char *image, int x_size, int y_size, 
         float red_factor, float green_factor, float blue_factor) {
 
         printf("Using adjust_channels() implementation #4 - ACLE (intrinsics for SVE2)\n");
-        printf("Coming soon!\n");
+
+/*
+
+        This is an ACLE (Arm C Language Extensions) (C intrinsics) version of
+        Implementation 2, above.
+        
+*/
+
+        svuint8_t       red_data, green_data, blue_data;        // data vectors for colour
+        svuint8x3_t     pixels = svcreate3(red_data, green_data, blue_data);     // tuple of 3 data vectors
+
+        uint64_t        lanes = svcntb();                       // count of data lanes
+        int             i=0;                                    // iterator
+        int             size = x_size * y_size * 3;             // image array size in bytes
+        svbool_t        p;                                      // predicate for load/store
+
+        svuint8_t       r = svdup_u8((uint8_t)(red_factor * 64.0));    // vector register with duplicated red_factor
+        svuint8_t       g = svdup_u8((uint8_t)(green_factor * 64.0));  // vector register with duplicated green_factor
+        svuint8_t       b = svdup_u8((uint8_t)(blue_factor * 64));     // vector register with duplicated blue_factor
+
+        svuint16_t      zero = svdup_u16(0);                    // vector register with duplicated zeros
+        svuint8_t       zerob = svdup_u8(0);                    // vector register with duplicated zeros
+
+        svuint16_t      tmp;                                    // vectors for temporary math values
+        svuint8_t       tmp_out;        
+        
+        // Diagnostic output
+//      printf("\n");
+//      for (int e = 0; e < 3000; e += 3) {
+//              printf("e:%3d   r:%3d   g:%3d   b:%3d\n", e, image[e], image[e+1], image[e+2]);
+//      }
+
+        for (i=0; i<size; i += lanes) {
+        
+//              printf("i:%d  ", i);
+
+                // ========= Load data
+                p = svwhilelt_b8(i, size);                      // get predicate value
+                pixels = svld3(p, image + i);                   // load tuple with image data
+                
+                // ========= Proccess channels
+                // --------- Red channel
+                tmp = svmullb_u16(svget3(pixels, 0), r);        // multiply data by factor, widen to 16-bit
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp_out = svaddhnb(tmp, zero);                  // narrow to 8-bit          
+
+                tmp = svmullt(svget3(pixels, 0), r);            // multiply data by factor, widen to 16-bit
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                pixels = svset3(pixels, 0, svaddhnt(tmp_out, tmp, zero));// narrow to 8-bit
+                
+                // --------- Green channel
+                tmp = svmullb(svget3(pixels, 1), g);            // multiply data by factor, widen to 16-bit
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp_out = svaddhnb(tmp, zero);                  // narrow to 8-bit          
+
+                tmp = svmullt(svget3(pixels, 1), g);            // multiply data by factor, widen to 16-bit
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                pixels =  svset3(pixels, 1, svaddhnt(tmp_out, tmp, zero));// narrow to 8-bit
+                
+                // --------- Blue channel
+                tmp = svmullb(svget3(pixels, 2), b);            // multiply data by factor, widen to 16-bit
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp_out = svaddhnb(tmp, zero);                  // narrow to 8-bit          
+
+                tmp = svmullt(svget3(pixels, 2), b);            // multiply data by factor, widen to 16-bit
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                tmp = svqadd(tmp, tmp);                         // double with saturation
+                pixels = svset3(pixels, 2, svaddhnt(tmp_out, tmp, zero));// narrow to 8-bit
+
+                // ========= Save data
+                svst3(p, image + i, pixels);                    // store tuple to image
+                                
+        }
+
+        // Diagnostic output
+//      printf("\n");
+//      for (int e = 0; e < 3000; e += 3) {
+//              printf("e:%3d   r:%3d   g:%3d   b:%3d\n", e, image[e], image[e+1], image[e+2]);
+//      }
 
 }
 
